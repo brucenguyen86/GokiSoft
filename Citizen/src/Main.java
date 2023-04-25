927f36323c3c013684fceeb76a5d6bd74b6e98ec
brucenguyen86 <130854166+brucenguyen86@users.noreply.github.com>
2023-04-25 10:25:22 +0700

    Hung branch commit

--- Citizen/src/Main.java
+++ Citizen/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) throws SQLException {
     Configs configs = new Configs();
     configs.input();
+    //Hello I want to test this code
+        // I have coded a lot, but there no changes on Github why?
 
     }
 }
